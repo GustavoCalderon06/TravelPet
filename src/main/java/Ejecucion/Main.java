e3b60eeb7e3c046cca224854e3d6600bf080b2a4
Lean9090P <l.pino06@ufromail.cl>
2022-09-16 20:23:15 -0300

    Commit 1 Leandro, main y menú

---
+++ src/main/java/Ejecucion/Main.java
@@ -0,0 +1,9 @@
+package Ejecucion;
+import model.MenuPrincipal;
+
+public class Main {
+    public static void main(String[] args) {
+        MenuPrincipal iniciar = new MenuPrincipal();
+    }
+
+}
