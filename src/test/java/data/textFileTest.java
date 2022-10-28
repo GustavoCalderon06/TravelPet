7f8fd045c40ac810b7a0df80a6fe933af0f916fa
Bernardo Sandoval <81835082+BernaHD18@users.noreply.github.com>
2022-10-28 20:17:37 -0300

    Add files via upload

--- src/test/java/data/textFileTest.java
+++ src/test/java/data/textFileTest.java
@@ -1,12 +1,7 @@
 package data;
 
 import org.junit.jupiter.api.Test;
-
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.HashMap;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,11 +9,11 @@ class textFileTest {
 
     @Test
     void leerArchivo()  {
-        ArrayList<String> actual = textFile.leerArchivo("src/test/java/data/readerTest");
+        ArrayList<String> actual = FileReader.leerArchivo("src/test/java/data/readerTest");
         ArrayList<String> expected = new ArrayList<>();
         expected.add("hola");
         assertEquals(expected, actual);
 
-
     }
+
 }
