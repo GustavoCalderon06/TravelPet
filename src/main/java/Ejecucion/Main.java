8c6915904a2603622159c76044a66472fc6385a8
Bernardo Sandoval <81835082+BernaHD18@users.noreply.github.com>
2022-10-28 20:14:08 -0300

    Add files via upload

---
+++ src/main/java/Ejecucion/Main.java
@@ -0,0 +1,14 @@
+package Ejecucion;
+
+import GUIs.VentanaPrincipal;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        VentanaPrincipal iniciar = new VentanaPrincipal();
+
+
+    }
+
+}
---
+++ src/main/java/data/DataUpdater.java
@@ -0,0 +1,69 @@
+package data;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DataUpdater {
+    public static void guardarUsuario(String usuario, String pass, String filepath, int id){
+        BufferedWriter bw = null;
+        FileWriter fichero = null;
+
+        try {
+            File file = new File(filepath);
+            fichero = new FileWriter(file.getAbsoluteFile(), true);
+            bw = new BufferedWriter(fichero);
+            if(id == 1){
+                bw.write(usuario+","+pass);
+            }
+            else {
+                bw.write("\n" + usuario + "," + pass);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //Cierra instancias de FileWriter y BufferedWriter
+                if (bw != null)
+                    bw.close();
+                if (fichero != null)
+                    fichero.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+    public static void guardarCurso(String nombreCurso, String filepath, int id){
+        BufferedWriter bw = null;
+        FileWriter fichero = null;
+
+        try {
+            File file = new File(filepath);
+            fichero = new FileWriter(file.getAbsoluteFile(), true);
+            bw = new BufferedWriter(fichero);
+            if(id == 1){
+                bw.write(nombreCurso);
+            }
+            else {
+                bw.write("\n" + nombreCurso);
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //Cierra instancias de FileWriter y BufferedWriter
+                if (bw != null)
+                    bw.close();
+                if (fichero != null)
+                    fichero.close();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+
+}
